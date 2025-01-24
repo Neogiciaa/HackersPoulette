2e32cb03b6c527fa31d3450a5d8b3814a2b767cc
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-01-24 11:06:50 +0100

    updated form style

--- index.php
+++ index.php
@@ -13,24 +13,41 @@ global $connexion;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="form.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <title>Hackers poulette</title>
 </head>
 <body>
-    <form>
-        <label for="name">Nom :</label>
-        <input id="name" type="text" placeholder="John">
+    <h1>Hackers Poulette SAV</h1>
+    <h2>Pour toute difficultée rencontrée, merci de nous contacter via le formulaire suivant !</h2>
 
-        <label for="firstname">Prénom :</label>
-        <input id="firstname" type="text" placeholder="Doe">
+    <form method="POST" action="">
+        <div>
+            <label for="name">Nom :</label>
+            <input id="name" type="text" placeholder="John" required>
+        </div>
 
-        <label for="email">Nom :</label>
-        <input id="email" type="text" placeholder="johndoe@gmail.com">
+        <div>
+            <label for="firstname">Prénom :</label>
+            <input id="firstname" type="text" placeholder="Doe" required>
+        </div>
 
-        <label for="file">Importez votre fichier :</label>
-        <input id="file" type="file">
 
-        <label for="description">Description :</label>
-        <input id="description" type="text" placeholder="Blabla">
+        <div>
+            <label for="email">Email :</label>
+            <input id="email" type="text" placeholder="johndoe@gmail.com" required>
+        </div>
+
+        <div class="description">
+            <label for="description">Description :</label>
+            <textarea id="description" placeholder="Décrivez votre problème..." required></textarea>
+        </div>
+
+        <div class ="import">
+            <label for="file">Importez votre fichier :</label>
+            <input id="file" type="file" required>
+        </div>
+
+        <input type="submit" value="Envoyer">
     </form>
 </body>
 </html>
