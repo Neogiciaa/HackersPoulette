dddf9d206b5add69931b5dff4d779179258b808e
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-01-24 14:20:57 +0100

    Added success page and style

---
+++ pages/success.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../styles/success.css">
+    <title>Votre demande à bien été prise en compte.</title>
+</head>
+<body>
+    <a href="index.php">
+        <img src="../public/houra.webp" alt="Houra">
+    </a>
+    <h1>Merci pour votre retour, nous traiterons votre demande dans les plus brefs délais.</h1>
+</body>
+</html>
